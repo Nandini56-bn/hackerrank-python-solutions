7f37e77b3959b001c96961b6945753cae1eac347
Nandini56-bn <nandini260719@gmail.com>
2026-03-10 22:36:02 +0530

    added hackerrank problems

---
+++ Hackerrank/filter.py
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+
+from functools import reduce
+l=[1,2,3,4,5,6]
+result=reduce(lambda x,y:x+y,l)
+print(result)
+
+=======
+l=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(list(filter(lambda n:n%2==0,range(1,11))))
+>>>>>>> 1e41ededd8b5845eeba364065325a6a8b831f46a
