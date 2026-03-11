28a968919905e51c02439b4343e811ff2fa60bb0
Nandini56-bn <nandini260719@gmail.com>
2026-03-11 19:19:49 +0530

    Delete pythonprograms directory

--- pythonprograms/file1.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-    arr = list(map(int, input().split()))
-
-    arr = list(set(arr))   # remove duplicates
-    arr.sort()             # sort list
-
-    print(arr[-2])         # second
